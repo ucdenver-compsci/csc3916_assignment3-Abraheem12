84b5d7a88ddd9bde0f4439c410c16d398a1caa78
Abraheem12 <abraheem.zadron@ucdenver.edu>
2024-04-10 13:24:43 -0600

    db updated

--- Users.js
+++ Users.js
@@ -1,47 +1,45 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
-var bcrypt = require('bcrypt-nodejs');
+var bcrypt = require('bcrypt');
 
 mongoose.Promise = global.Promise;
 
-mongoose.connect(process.env.DB, { useNewUrlParser: true });
-try {
-    mongoose.connect( process.env.DB, {useNewUrlParser: true, useUnifiedTopology: true}, () =>
-        console.log("connected"));
-}catch (error) {
-    console.log("could not connect");
-}
+// Connect to the MongoDB database
+mongoose.connect(process.env.DB, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log("Connected to MongoDB"))
+    .catch(err => console.error("Error connecting to MongoDB:", err));
+
 mongoose.set('useCreateIndex', true);
 
-//user schema
+// Define user schema
 var UserSchema = new Schema({
     name: String,
-    username: { type: String, required: true, index: { unique: true }},
+    username: { type: String, required: true, unique: true },
     password: { type: String, required: true, select: false }
 });
 
-UserSchema.pre('save', function(next) {
-    var user = this;
+// Hash the password before saving to the database
+UserSchema.pre('save', async function(next) {
+    try {
+        if (!this.isModified('password')) return next();
 
-    //hash the password
-    if (!user.isModified('password')) return next();
-
-    bcrypt.hash(user.password, null, null, function(err, hash) {
-        if (err) return next(err);
-
-        //change the password
-        user.password = hash;
+        const salt = await bcrypt.genSalt(10);
+        const hash = await bcrypt.hash(this.password, salt);
+        this.password = hash;
         next();
-    });
+    } catch (error) {
+        next(error);
+    }
 });
 
-UserSchema.methods.comparePassword = function (password, callback) {
-    var user = this;
+// Method to compare passwords
+UserSchema.methods.comparePassword = async function (password) {
+    try {
+        return await bcrypt.compare(password, this.password);
+    } catch (error) {
+        return false;
+    }
+};
 
-    bcrypt.compare(password, user.password, function(err, isMatch) {
-        callback(isMatch);
-    })
-}
-
-//return the model to server
+// Return the model to the server
 module.exports = mongoose.model('User', UserSchema);

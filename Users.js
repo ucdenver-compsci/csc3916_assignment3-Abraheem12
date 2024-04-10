df9715e26ab2f9795a5dc1e5a8b3834ae9e0ce24
Abraheem12 <abraheem.zadron@ucdenver.edu>
2024-04-10 13:58:26 -0600

    updation

--- Users.js
+++ Users.js
@@ -4,14 +4,14 @@ var bcrypt = require('bcrypt-nodejs');
 
 mongoose.Promise = global.Promise;
 
-
-
-
-mongoose.connect(process.env.DB)
-
-
-
-
+//mongoose.connect(process.env.DB, { useNewUrlParser: true });
+try {
+    mongoose.connect( process.env.DB, {useNewUrlParser: true, useUnifiedTopology: true}, () =>
+        console.log("connected"));
+}catch (error) {
+    console.log("could not connect");
+}
+mongoose.set('useCreateIndex', true);
 
 //user schema
 var UserSchema = new Schema({
